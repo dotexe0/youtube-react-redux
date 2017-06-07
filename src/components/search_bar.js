ed5422691bc4687bcd126b328b5821f9f6bab5b3
dotexe0 <dotexe0@gmail.com>
2017-06-06 19:14:25 -0700

    added basic components

---
+++ src/components/search_bar.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {query: ''};
+
+    }    
+
+    render() {
+        return (
+            <div>
+                <input 
+                    value={this.state.query}
+                    onChange={ e => this.setState({query: e.target.value})} />
+                Value of Input: {this.state.query}
+            </div>
+        );
+    }
+
+    // _onInputChange = (e) => {
+    //     console.log(e.target.value);
+    // }
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
+// create new component that will produce html
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
+const API_KEY = 'AIzaSyAd8PgOnrQ0JMcXu0p6mLOPGY6xf4y9MZ8';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const App = () => {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
+}
+
+
+//take this component's generated html and render it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
