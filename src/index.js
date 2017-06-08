6f88e84880a361fcdbc837d76bf1c1085710771f
dotexe0 <dotexe0@gmail.com>
2017-06-07 18:59:24 -0700

    working videos api, need to add click handling

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 // create new component that will produce html
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail'
 const API_KEY = 'AIzaSyAd8PgOnrQ0JMcXu0p6mLOPGY6xf4y9MZ8';
 
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            videos: []
+        }
+
+        YTSearch({key: API_KEY, query: 'ethereum'}, (videos) => {
+            this.setState({ videos }); // this.setState({ videos: videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.videos[0]} />
+                <VideoList videos={this.state.videos}/>
+            </div>
+        );
+    }
 }
 
 
